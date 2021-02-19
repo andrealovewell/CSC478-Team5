1f634442228a4f5734fc7e4385e6c8e5c94530b8
andrealovewell <andy.lovewell@gmail.com>
2021-02-19 07:19:55 -0800

    Pre Sign Up Trigger
    preTrigger Event Lamda Code used in Cognito

---
+++ Backend/preSignUpTrigger.py
@@ -0,0 +1,9 @@
+import json
+
+
+def lambda_handler(event, context):
+    event['response']['autoConfirmUser'] = True
+    event['response']['autoVerifyPhone'] = True
+    event['response']['autoVerifyEmail'] = True
+
+    return event
