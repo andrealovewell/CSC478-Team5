520bd8a27e084cbe1519ad7c9309e29c8dae1df3
andrealovewell <andy.lovewell@gmail.com>
2021-02-19 21:08:24 -0800

    Updated PreSign Trigger
    changed the boolean for autoverifyphone from true to false

--- Backend/preSignUpTrigger.py
+++ Backend/preSignUpTrigger.py
@@ -3,7 +3,7 @@ import json
 
 def lambda_handler(event, context):
     event['response']['autoConfirmUser'] = True
-    event['response']['autoVerifyPhone'] = True
+    event['response']['autoVerifyPhone'] = False
     event['response']['autoVerifyEmail'] = True
 
     return event
