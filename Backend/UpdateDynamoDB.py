54ce39584019b5c467fd71fec1504a65c50911f1
andrealovewell <andy.lovewell@gmail.com>
2021-03-11 20:26:52 -0800

    Update DynamoDB code
    Code to update records in the database

---
+++ Backend/UpdateDynamoDB.py
@@ -0,0 +1,50 @@
+import json
+# import the json utility package since we will be working with a JSON object
+import json
+# import the AWS SDK (for Python the package name is boto3)
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+import decimal
+
+# create a DynamoDB object using the AWS SDK
+dynamodb = boto3.resource('dynamodb')
+# use the DynamoDB object to select our table
+table = dynamodb.Table('JAMDB')
+
+
+# handler for the lambda function
+def lambda_handler(event, context):
+    # extract values from the event object we got from the Lambda service and store in a variable
+    user = event['email']
+    appID = event['appID']
+    company = event['company']
+    title = event['position']
+    progress = event['progress']
+    location = event['location']
+    weblink = event['weblink']
+    description = event['description']
+    requirement = event['requirement']
+    references = event['references']
+    notes = event['notes']
+
+    response = table.update_item(
+        Key={
+            'User_Id': user,
+            'App_Id': appID,
+        },
+
+        UpdateExpression='SET Company=:C, Title=:T, Progress=:P, JobLocation=:L, Weblink=:W, JobDescription= :Jdesc, JobRequirements= :Jreq, PersonalReferences=:R, Notes =:N',
+
+        ExpressionAttributeValues={
+            ':C': company,
+            ':T': title,
+            ':P': progress,
+            ':L': location,
+            ':W': weblink,
+            ':Jdesc': description,
+            ':Jreq': requirement,
+            ':R': references,
+            ':N': notes
+        },
+        ReturnValues="UPDATED_NEW"
+    )
